06c656b9aa427d29369d39de06c6328e6c27fab3
Nick James <njames.dev@gmail.com>
2025-03-31 13:58:15 -0400

    Make changes to the GPS custom field:
    - Remove the functionality of autofilling the location
    - Make the latitude and longitude fields read only

--- src/components/Gps/Gps.edit.display.js
+++ src/components/Gps/Gps.edit.display.js
@@ -1,9 +1,1 @@
-export default [
-  {
-    type: "checkbox",
-    key: "defaultToCurrentLocation",
-    label: "Default to Current Location",
-    tooltip: "Automatically fetch GPS coordinates when the form loads.",
-    input: true,
-  },
-];
+export default [];
