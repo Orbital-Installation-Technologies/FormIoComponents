1557d4345dc901539278da846e317a159f60e054
Daniel9238 <92Daniel38@gmail.com>
2025-06-12 14:29:36 -0500

    chore: add checkbox

---
+++ src/patchAllComponents.js
@@ -0,0 +1,25 @@
+import AllComponents from "@formio/components";
+
+Object.values(AllComponents).forEach((Component) => {
+  if (Component && typeof Component.editForm === "function") {
+    const originalEditForm = Component.editForm;
+
+    Component.editForm = (...extend) => {
+      const form = originalEditForm(...extend);
+
+      const displayTab = form.find((section) => section.key === "display");
+      if (displayTab && !displayTab.components.some((c) => c.key === "reviewVisible")) {
+        displayTab.components.push({
+          type: "checkbox",
+          key: "reviewVisible",
+          label: "Show in Review Modal",
+          input: true,
+          weight: 999,
+          tooltip: "If checked, this field will appear in the Review modal summary.",
+        });
+      }
+
+      return form;
+    };
+  }
+});
