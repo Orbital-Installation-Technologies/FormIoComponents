1aa379874cc9185e5c9a46553272db1ddd14785b
joe <20josephross@gmail.com>
2025-07-15 16:34:11 -0500

    Added Premium components Licensing

--- webpack.prod.cjs
+++ webpack.prod.cjs
@@ -1,4 +1,6 @@
 const path = require("path");
+const webpack = require("webpack");
+const Dotenv = require('dotenv-webpack');
 
 module.exports = (env) => {
   const buildNumber = env.buildNumber || "dev"; // Default to "dev" if no build number is provided
@@ -8,10 +10,15 @@ module.exports = (env) => {
     entry: {
       components: "./src/app-prod.js",
     },
+    plugins: [
+      new Dotenv(),
+      new webpack.EnvironmentPlugin(["FORMIO_PREMIUM_LICENSE"]),
+    ],
     output: {
       filename: `[name].bundle.${buildNumber}.js`,
       path: path.resolve(__dirname, "dist"),
       clean: true,
     },
+    // No runtime process polyfill needed; Define via EnvironmentPlugin at build time
   };
 };
