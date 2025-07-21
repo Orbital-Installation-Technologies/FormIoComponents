3bfc2b6bf5f6acf506ccb16149df30e30212344c
joe <josephross@makoonstech.com>
2025-07-21 13:48:22 -0500

    removing changes for premium licensing that's no longer needed.

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -1,7 +1,5 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const webpack = require("webpack");
-const Dotenv = require('dotenv-webpack');
 
 module.exports = {
   mode: "development",
@@ -9,20 +7,11 @@ module.exports = {
     index: "./src/app-dev.js",
   },
   plugins: [
-    new Dotenv(),
-    new webpack.DefinePlugin({
-      'process': JSON.stringify({
-        env: {
-          //FORMIO_PREMIUM_LICENSE: process.env.FORMIO_PREMIUM_LICENSE || ''
-        }
-      })
-    }),
     new HtmlWebpackPlugin({
       title: "output management",
       template: "./template.html",
     }),
   ],
-  // No runtime process polyfill needed; DefinePlugin replaces env vars at compile time
   devtool: "inline-source-map",
   output: {
     filename: "[name].bundle.js",
