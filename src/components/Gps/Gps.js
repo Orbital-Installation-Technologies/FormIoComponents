872647f5b7f66360033c492faa5398d6b3c4bb18
abhilash-aot <abhilash.kr@aot-technologies.com>
2026-02-11 00:38:45 -0800

    Add console log for value update testing
    Added a console log for testing purposes when the value changes.

--- src/components/Gps/Gps.js
+++ src/components/Gps/Gps.js
@@ -353,7 +353,7 @@ export default class Gps extends FieldComponent {
     const changed = this.dataValue !== value;
     if (changed) {
       super.setValue(value, flags);
-
+      console.log("for testing update....");
       if (!flags.noUpdateEvent) {
         this.triggerChange();
       }
