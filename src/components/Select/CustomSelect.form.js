9b5575ed3ab544a711b020935f2ca81aec0c26f7
abhilash-aot <abhilash.kr@aot-technologies.com>
2026-03-17 09:41:43 -0700

    added custom select wrap for options

--- src/components/Select/CustomSelect.form.js
+++ src/components/Select/CustomSelect.form.js
@@ -1,7 +1,22 @@
 import { Components } from "@formio/js";
 export default function (...extend) {
   return Components.baseEditForm(
-
+    [
+      {
+        key: 'display',
+        components: [
+          {
+            type: 'checkbox',
+            key: 'wrapOptionText',
+            label: 'Wrap long option text',
+            tooltip: 'When enabled, option text that exceeds the width of the dropdown will wrap onto additional lines instead of being truncated.',
+            input: true,
+            defaultValue: true,
+            weight: 700,
+          }
+        ]
+      }
+    ],
     ...extend,
   );
 }
